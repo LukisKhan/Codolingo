3fe097763a668f51b40395974efaeeacb999e892
kmoonwright <kmoonwright@outlook.com>
2019-10-06 16:26:15 -0700

    Refactor of existing code for clarity

--- client/src/components/nav/SplashNav.js
+++ client/src/components/nav/SplashNav.js
@@ -10,39 +10,39 @@ const { IS_LOGGED_IN } = Queries;
 const SplashNav = props => {
     return (
       <div>
-    <ApolloConsumer>
-      {client => (
-        <Query query={IS_LOGGED_IN}>
-          {({ data }) => {
-            if (data.isLoggedIn) {
-              return (
-                <div>
-                  <button
-                    onClick={e => {
-                      e.preventDefault();
-                      localStorage.removeItem("auth-token");
-                      client.writeData({ data: { isLoggedIn: false } });
-                      props.history.push("/");
-                    }}
-                  >
-                    Logout
-                  </button>
-                  <Link to="/courses">Courses</Link>
-                </div>
-              );
-            } else {
-              return (
-                <div>
-                  <Link to="/">Home</Link>
-                  <Link to="/login">Login</Link>
-                  <Link to="/register">Register</Link>
-                </div>
-              );
-            }
-          }}
-        </Query>
-      )}
-    </ApolloConsumer>
+        <ApolloConsumer>
+          {client => (
+            <Query query={IS_LOGGED_IN}>
+              {({ data }) => {
+                if (data.isLoggedIn) {
+                  return (
+                    <div>
+                      <button
+                        onClick={e => {
+                          e.preventDefault();
+                          localStorage.removeItem("auth-token");
+                          client.writeData({ data: { isLoggedIn: false } });
+                          props.history.push("/");
+                        }}
+                      >
+                        Logout
+                      </button>
+                      <Link to="/courses">Courses</Link>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div>
+                      <Link to="/">Home</Link>
+                      <Link to="/login">Login</Link>
+                      <Link to="/register">Register</Link>
+                    </div>
+                  );
+                }
+              }}
+            </Query>
+          )}
+        </ApolloConsumer>
       </div>
   )
 }
