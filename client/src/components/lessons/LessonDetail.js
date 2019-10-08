5f0cd609fd37c6540070a428743e8047a3c0261f
Luat Pham <luat32@gmail.com>
2019-10-08 15:02:30 -0500

    Added lesson detail page showing questions and answers

--- client/src/components/lessons/LessonDetail.js
+++ client/src/components/lessons/LessonDetail.js
@@ -7,7 +7,18 @@ const { FETCH_LESSON } = Queries;
 
 class LessonDetail extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = { questionIdx: 0 }
+  }
+  chooseAnswer(e, isCorrect){
+    // e.preventDefault();
+    e.stopPropagation();
+    // console.log(e.target.value);
+    if (isCorrect){
+      console.log("Correct");
+    } else {
+      console.log("Try again");
+    }
   }
   render() {
     return (
@@ -16,35 +27,34 @@ class LessonDetail extends React.Component {
             {({ loading, error, data }) => {
               if (loading) return <p>Loading...</p>;
               if (error) return <p>Error</p>;
-              return (
-                <div>
-                  <p>Lesson details</p>
-                  <p>Title: {data.lesson.title}</p>
-                  <p>ID: {data.lesson._id}</p>
-                  <ul>
-                    {data.lesson.questions.map((question) => {
-                      return (
-                        <li key={question._id}>
-                            <h4>{question.prompt}</h4>
-                          <ul>
-                            {question.answers.map((answer) => {
-                              console.log(answer);
-                              return (
-                                  <li key={answer._id}>
-                                      <h4>{answer.answer}</h4>
-                                      <h4>Correct: {answer.isCorrect.toString()}</h4>
-                                  </li>
-                                )
-                              })
-                            }
-                          </ul>
-                        </li>
-                      )
-                    }
-                  )}
-                  </ul>
-                </div>
-              )
+              let questionArray = data.lesson.questions;
+              let questionCurrent = questionArray[this.state.questionIdx];
+              console.log(this.state.questionIdx);
+              if (this.state.questionIdx >= questionArray.length) {
+                return <div>End of lesson</div>
+              } else {
+                return (
+                  <div>
+                    <p>Lesson details</p>
+                    <p>Title: {data.lesson.title}</p>
+                    <p>ID: {data.lesson._id}</p>
+                      <p>What would the follow expression return: </p>
+                      <p>{questionCurrent.prompt}</p>
+                      <ul>
+                        {questionCurrent.answers.map((answer) => {
+                          return (
+                            <li key={answer._id} onClick={e => this.chooseAnswer(e, answer.isCorrect)}>
+                              <h4>{answer.answer}</h4>
+                              {/* <h4>Correct: {answer.isCorrect.toString()}</h4> */}
+                            </li>
+                          )
+                        })}
+                      </ul>
+                    <button onClick={e => { this.setState({ questionIdx: this.state.questionIdx + 1 }) }}>Next</button>
+                  </div>
+                )
+
+              }
             }}
           </Query>
 
