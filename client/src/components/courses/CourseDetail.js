f6dbd51900ffbbab0c4aa91d90cfc5acbab7b9d7
Luat Pham <luat32@gmail.com>
2019-10-13 20:03:29 -0500

    Debugged course detail page to show specific lessons

--- client/src/components/courses/CourseDetail.js
+++ client/src/components/courses/CourseDetail.js
@@ -3,22 +3,23 @@ import { Query } from "react-apollo";
 import { Link, withRouter } from "react-router-dom";
 import Queries from "../../graphql/queries";
 import CourseHeader from "./CourseHeader";
-const { FETCH_LESSONS, FETCH_COURSE } = Queries;
+const { FETCH_LESSONS, FETCH_COURSES } = Queries;
 
 class CourseDetail extends React.Component {
     render() {
-        console.log(this.props);
         return (
             <div >
                 <CourseHeader courseId={this.props.match.params.id} />
                 <div className="courseTitle">{this.props.match.params.id}</div>
                 <div className="lesson-list-page">
                     <ul className="lesson-list">
-                        <Query query={FETCH_COURSE} variables={{id: this.props.location.state.id}}>
+                        <Query query={FETCH_COURSES}>
                             {({ loading, error, data }) => {
                                 if (loading) return <p>Loading...</p>;
                                 if (error) return <p>Error</p>;
-                                console.log(data);
+                                console.log(this.props.match.params.id);
+                                console.log(data.courses);
+                                data.course = data.courses.filter(course => course.language === this.props.match.params.id)[0];
                                 return data.course.lessons.map(({ _id, title }) => (
                                     <li key={_id}>
                                         <Link className="lesson-link" to={`/lessons/${_id}`}>
