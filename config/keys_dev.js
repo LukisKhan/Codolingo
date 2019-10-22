3b29521547e3120b99a806dd6670193fc94e8ce6
Brett Meyer <meyer.brett.da@gmail.com>
2019-10-22 14:31:27 -0700

    stuff

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URI: "mongodb+srv://luatDB:hunter12@cluster0-spdpb.mongodb.net/codolingo?retryWrites=true&w=majority",
+    secretOrKey: "53921650-2c26-4ef1-bb39-73a5cf2dafa3"
+};
