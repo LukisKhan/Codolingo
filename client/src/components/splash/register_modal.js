ef625b27835cd1d817cb4f2cbe8de1bdebde601e
kmoonwright <kmoonwright@outlook.com>
2019-11-04 15:56:05 -0800

    Add demo login with randomizer

--- client/src/components/splash/register_modal.js
+++ client/src/components/splash/register_modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Mutation} from 'react-apollo';
-import {REGISTER_USER} from '../../graphql/mutations';
+import {REGISTER_USER, LOGIN_USER} from '../../graphql/mutations';
 import { withRouter } from 'react-router-dom';
 
 import Close from '../../assets/close.svg';
@@ -67,6 +67,17 @@ class RegisterModal extends React.Component {
                             </div>
                             <button className="authButton" type="submit">Sign up</button>
                         </form>
+                        <button className="authButton"                         
+                            onClick={e => {
+                                e.preventDefault();
+                                registerUser({
+                                    variables: {
+                                        name: (Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)),
+                                        email: (Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)) + "@email.com",
+                                        password: 'password'
+                                    }
+                                });
+                            }}>Demo Login</button>
         
                         <div className="disclaimer">
                             <h5>By signing in to Codolingo, you agree to our Terms and Privacy Policy</h5>
